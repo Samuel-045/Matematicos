900aaae6681250c91baad2736853ea4107b26a40
Samuel-045 <samuelvic856@gmail.com>
2024-12-02 21:08:29 -0300

    Links adicionados no side-bar

--- src/components/menuSideBar/index.jsx
+++ src/components/menuSideBar/index.jsx
@@ -1,6 +1,7 @@
 import './index.scss'
 import { SlArrowRight } from "react-icons/sl";
 import { BiX } from "react-icons/bi";
+import { Link } from 'react-router-dom';
 
 export default function MenuSB({visibleMenu_}){
 
@@ -8,9 +9,9 @@ export default function MenuSB({visibleMenu_}){
         <div className="main-menu" >
             <BiX className="icon-x" onClick={visibleMenu_}/>
             <ul>
-                <li><SlArrowRight className="icon-itens"/> Home</li>
-                <li><SlArrowRight className="icon-itens"/> Calculadora</li>
-                <li><SlArrowRight className="icon-itens"/> Contato</li>
+                <li><Link to="/" className='link'><SlArrowRight className="icon-itens"/> Home </Link></li>
+                <li><Link to="/calculadora" className='link'><SlArrowRight className="icon-itens"/> Calculadora</Link></li>
+                <li><Link to="/contato" className='link'><SlArrowRight className="icon-itens"/> Contato</Link></li>
             </ul>
         </div>
     );
