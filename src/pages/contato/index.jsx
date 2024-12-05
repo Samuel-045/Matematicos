7628754ced774c91cda1b7268fad46ff90174941
Samuel-045 <samuelvic856@gmail.com>
2024-12-04 23:50:45 -0300

    Criação do conteúdo da tela de contato

--- src/pages/contato/index.jsx
+++ src/pages/contato/index.jsx
@@ -1,5 +1,7 @@
 import "./index.scss"
 import Header from "../../components/header"
+import Titulo from "../../components/artigosTitulo"
+import Paragrafo from "../../components/artigosConteudo"
 
 export default function contato(){
     return(
@@ -8,7 +10,16 @@ export default function contato(){
                 <Header/>
             </header>
             
-            <p>contato</p>
+            <main>
+                <Titulo titulo={"Formas de contato"}  />
+
+                <Paragrafo conteudo={"Para entrar em contato com a nossa equipe há os seguintes canais de contato: "}/>
+                <Paragrafo conteudo={"E-mail: matMaticos@gmail.com"} />
+                <Paragrafo conteudo={"Telefone : (15)98756-9081"} />
+                <Paragrafo conteudo={"Telefone : (15)79736-9551"} />
+                <Paragrafo conteudo={"Telefone : (15)59146-7521"} />
+                <Paragrafo conteudo={"Será um prazer poder sanar suas dúvidas e poder te ajudar!"} />
+            </main>
         </div>
     )
 }
